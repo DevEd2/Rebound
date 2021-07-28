b5e3fc54873955354d96051e35f4ff0d97f4aaab
DevEd <deved8@gmail.com>
2021-07-27 22:48:04 -0400

    updated central city theme draft

---
+++ Engine/Credits.asm
@@ -0,0 +1,48 @@
+GM_Credits:
+    ; TODO
+    ret
+
+; ================================================================
+
+; Subject to change as development continues
+CreditsText:
+;            ####################
+    db      "                    "
+    db      "    - REBOUND -     "
+    db      "  A game  by DevEd  "
+    db      "                    "
+    db      " Lead programmer:   "
+    db      "              DevEd "
+    db      "                    "
+    db      " Programmers:       "
+    db      "              Arkia "
+    db      "                    "
+    db      " Graphics:          "
+    db      "          Twoflower "
+    db      "              DevEd "
+    db      "                    "
+    db      " Music composition: "
+    db      "              DevEd "
+    db      "                    "
+    db      " Sound programming: "
+    db      "              DevEd "
+    db      "                ??? "
+    db      "                    "
+    db      " Tools used:        "
+    db      "                BGB "
+    db      "            Tilekit "
+    db      "          DefleMask "
+    db      "              RGBDS "
+    db      "                    "
+    db      " Special thanks:    "
+    db      "       Gunpei Yokoi "
+    db      "    Hirokazu Tanaka "
+    db      "          Superogue "
+    db      "             ISSOtm "
+    db      "      Elmar Krieger "
+    db      "   Alberto Gonzalez "
+    db      "       Moviemovies1 "
+    db      "                    "
+    db      "     THANK  YOU     "
+    db      "    FOR PLAYING!    "
+    db      "                    "
