a5931da41d7ff2ddef18136639bda1da8f314433
unknown <deved8@gmail.com>
2021-09-18 14:46:29 -0400

    added press start text to title screen
    TODO: animate it

--- Engine/GameModes/TitleAndMenus.asm
+++ Engine/GameModes/TitleAndMenus.asm
@@ -1,5 +1,8 @@
 section "Title screen + menu RAM",wram0
 
+Title_PressStartX:	db
+Title_PressStartY:	db
+
 ; ================
 
 section "Title screen + menu routines",rom0
@@ -19,9 +22,19 @@ GM_TitleAndMenus:
 	ld		a,1
 	ldh		[rVBK],a
 	
-    ldfar   hl,TitleScreenTiles2
+    ld		hl,TitleScreenTiles2
     ld      de,$8000
     call    DecodeWLE
+	
+	ld		hl,Title_PressStartTiles
+	ld		de,$8270
+	ld		b,Title_PressStartTiles.end-Title_PressStartTiles
+:
+	ld		a,[hl+]
+	ld		[de],a
+	inc		e
+	dec		b
+	jr		nz,:-
 	
 	ld		hl,TitleScreenAttr
     ld      de,sys_TilemapBuffer
@@ -40,6 +53,10 @@ GM_TitleAndMenus:
 	inc		a
 	dec		b
 	jr		nz,:-
+	ld		hl,Pal_Title_PressStart
+	ld		a,8
+	call	LoadPal
+	
     call    ConvertPals
     call    PalFadeInWhite
     call    UpdatePalettes
@@ -48,11 +65,16 @@ GM_TitleAndMenus:
 	ldh		[rVBK],a
     farcall DevSound_Init
 
-    ld      a,LCDCF_ON | LCDCF_BG8000 | LCDCF_BGON
+    ld      a,LCDCF_ON | LCDCF_BG8000 | LCDCF_BGON |LCDCF_OBJON
     ldh     [rLCDC],a
     ld      a,IEF_VBLANK
     ldh     [rIE],a
-    ei
+    
+	ld		a,52
+	ld		[Title_PressStartX],a
+	ld		a,144
+	ld		[Title_PressStartY],a
+	ei
 
 TitleLoop:
     ld      a,[sys_btnPress]
@@ -85,16 +107,64 @@ TitleLoop:
     jp      GM_Level
 
 .skip
+	; draw "PRESS START" text
+	ld		hl,Metasprite_PressStart
+	ld		b,(Metasprite_PressStart.end-Metasprite_PressStart)/4
+	call	BeginSprites
+:
+	push	bc
+	ld		a,[hl+]
+	ld		d,a
+	ld		a,[Title_PressStartY]
+	add		d
+	ld		d,a
+	ld		a,[hl+]
+	ld		e,a
+	ld		a,[Title_PressStartX]
+	add		e
+	ld		e,a
+	ld		a,[hl+]
+	ld		b,a
+	ld		a,[hl+]
+	ld		c,a
+	push	hl
+	call	AddSprite
+	pop		hl
+	pop		bc
+	dec		b
+	jr		nz,:-
+	call	EndSprites
+
     halt
     jr      TitleLoop
     
+Metasprite_PressStart:
+	db	$00,$00,$27,%00001000
+	db	$00,$08,$28,%00001000
+	db	$00,$10,$29,%00001000
+	db	$00,$18,$2A,%00001000
+	db	$00,$20,$2B,%00001000
+	db	$00,$28,$2C,%00001000
+	db	$00,$30,$2D,%00001000
+	db	$00,$38,$2E,%00001000
+	db	$00,$40,$2F,%00001000
+.end
+	
 ; ================================================================
     
 section "Title screen + menu GFX",romx
 
-Pal_TitleScreen:	incbin	"GFX/TitleScreen.pal"
+Pal_TitleScreen:		incbin	"GFX/TitleScreen.pal"
 
-TitleScreenTiles1:  incbin  "GFX/TitleScreen_Block1.2bpp.wle"
-TitleScreenTiles2:  incbin  "GFX/TitleScreen_Block2.2bpp.wle"
-TitleScreenMap:     incbin  "GFX/TitleScreen.til.wle"
-TitleScreenAttr:    incbin  "GFX/TitleScreen.atr.wle"
+TitleScreenTiles1:  	incbin  "GFX/TitleScreen_Block1.2bpp.wle"
+TitleScreenTiles2:  	incbin  "GFX/TitleScreen_Block2.2bpp.wle"
+TitleScreenMap:     	incbin  "GFX/TitleScreen.til.wle"
+TitleScreenAttr:    	incbin  "GFX/TitleScreen.atr.wle"
+
+Title_PressStartTiles:	incbin	"GFX/Sprites/PressStart.2bpp"	; storing this uncompressed because the WLE "packer" decided to inflate it by 3 bytes instead
+.end
+Pal_Title_PressStart:
+	RGB	31, 0,31
+	RGB	 0, 0, 0
+	RGB 15,24,31
+	RGB	31,31,31
